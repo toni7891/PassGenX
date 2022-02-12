661d8d8048a6df51944e77bb2435baab6f953a76
4RCH3R <48356347+toni7891@users.noreply.github.com>
2022-02-12 23:27:50 +0200

    1.0

--- PassGenX.py
+++ PassGenX.py
@@ -1,54 +1,140 @@
 # PassGenX - A program to generate passwords and keep them safe.
+
+# Importing library's 
 import random
+import base64
+
+# Global variables
+dataBase_Dest = r"C:\Users\Tony\Desktop\passGen PROJECT\Encrypted_Data.txt"
+validationPass_Dest = r"C:\Users\Tony\Desktop\passGen PROJECT\Admin.txt"
+
+
 
 def main():
     menu()
     
-    
+
+   
 def menu():
     newPassCharNum = 0
-    menuMassage = "Welcome to PassGenX!\nPlease select what operation you want to execute:\n    1- Create new Pass\n    2- Encrypt existing Pass\n    3- forgot Pass [ID num required]\nYour chioce: "
+    menuMassage = """Welcome to PassGenX!
+    Please select what operation do you want to execute:
+    1- Create new Pass    
+    2- See existing Passwords
+    3- Erase Data file
+    Your choice: """
     operationID = int(input(menuMassage))
     
     if operationID == 1:
-        newPassLength = int(input("Please enter password lenght: "))
+        newPassLength = int(input("Please enter password length: "))
         NewpassGenerator(newPassLength)
+    
     elif operationID == 2:
-        existingEncrypt()
+        validationID = input("Please enter Admin Password: ")
+        if validateAdmin(validationID) == True:
+            decodeFile()
+        elif validateAdmin(validationID) == False:
+            print('Wrong admin password...\nget the Fuck out!')
         
-    """elif operationID == 3:
-        forgotPass()"""
+    elif operationID == 3:
+        eraseDataFile()
         
-    
-    1
-    
+    else: 
+        print("There is no such Operation ID...")        
+
     
 
 def NewpassGenerator(passLength):
     lowerLetterChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
     upperLetterChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     specialChars = ['!', '@', '#', '$', '%', '&', '*']
+    #special64Chars = ['+', '/']
     numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     newPassWord = ""
-    allChars = lowerLetterChars + upperLetterChars + specialChars + numberChars
+    allChars = lowerLetterChars + upperLetterChars + numberChars + specialChars
     #allChars = ''.join(allChars)
     for i in range(passLength):
         newPassWord = newPassWord + random.choice(allChars)
+    print('Your new password is: ' + newPassWord)
+    inFile(encryptionAlgo(newPassWord))
+    print('your password have been encrypted!')
     
-        
+    
+       
+def encryptionAlgo(password):
+    passBytes = password.encode('ascii')
+    encodedPass = base64.b64encode(passBytes)
+    return encodedPass
+
+def inFile(encoded_Data):
+       fileOpen = open(dataBase_Dest, "a")
+       fileOpen.write(str(encoded_Data) + '\n')
+       fileOpen.close() 
+       print('Your Password has been saved!')
+ 
+
+def decodeFile():
+    decodedData = []
+    decodedPass = ""
+    openFileFrom = open(dataBase_Dest, "r")
+    encryptedData = openFileFrom.read()
+    openFileFrom.close()
+
+    sortedData = encryptedData.split("'")
+    sortedData = base_64_b_fix(sortedData)
+
+    for i in range(len(sortedData)):
+        decodedPass = base64.b64decode(str(sortedData[i]))
+        decodedData.append(decodedPass)
+    for i in range(len(decodedData)):
+        decodedData[i] = str(decodedData[i]).split("'")
+    print('Saved passwords:')
+    for lists in decodedData:
+        print(lists[1])
+
+
+def validateAdmin(checkedPass):
+    openedPassFile = open(validationPass_Dest, "r")
+    passFileData = openedPassFile.read()
+    
+    sortedPassData = passFileData.split("'")
+    sortedPassData = base_64_b_fix(sortedPassData)
+    
+    actualPass = str(base64.b64decode(str(sortedPassData).encode("ascii")))
+    actualPass = actualPass.split("'")
+    actualPass = base_64_b_fix(actualPass)
+    
+    if actualPass[0] == str(checkedPass):
+        return True
+    else: 
+        return False
+
+
+def base_64_b_fix(list_to_clear):
+    list_to_clear = list(filter(lambda a: a != 'b', list_to_clear))
+    list_to_clear = list(filter(lambda a: a != '\nb', list_to_clear))
+    list_to_clear = list(filter(lambda a: a != '\n', list_to_clear))
+    list_to_clear = list(filter(lambda a: a != '', list_to_clear))
+    return list_to_clear
     
     
-    
-    
-"""
-def existingEncrypt():
-     
-def encryptionAlgo():
-    
-def forgotPass():
-"""
-    
+def eraseDataFile():
+    open(dataBase_Dest, "w").close()
 
 if __name__ == '__main__':
     main()
     
+"""
+coded by:
+         @@@  @@@@@@@   @@@@@@@ @@@  @@@ @@@@@@  @@@@@@@   
+        @@@@  @@@@@@@@ @@@@@@@@ @@@  @@@ @@@@@@@ @@@@@@@@  
+       @@!@!  @@!  @@@ !@@      @@!  @@@     @@@ @@!  @@@  
+      !@!!@!  !@!  @!@ !@!      !@!  @!@     @!@ !@!  @!@  
+     @!! @!!  @!@!!@!  !@!      @!@!@!@! @!@!!@  @!@!!@!   
+    !!!  !@!  !!@!@!   !!!      !!!@!!!! !!@!@!  !!@!@!    
+    :!!:!:!!: !!: :!!  :!!      !!:  !!!     !!: !!: :!!   
+    !:::!!::: :!:  !:! :!:      :!:  !:!     :!: :!:  !:!  
+         :::  ::   :::  ::: ::: ::   ::: :: :::: ::   :::  
+         :::   :   : :  :: :: :  :   : :  : : :   :   : :
+
+"""
