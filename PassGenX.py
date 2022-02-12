a78258f22584719673e23706de14183ec8d6610b
4RCH3R <48356347+toni7891@users.noreply.github.com>
2022-02-12 14:48:33 +0200

    init

---
+++ PassGenX.py
@@ -0,0 +1,23 @@
+# PassGenX - A program to generate passwords and keep them safe.
+
+def main():
+    menu()
+    
+    
+def menu():
+    menuMassage = "Welcome to PassGenX!\nPlease select what operation you want to execute:\n    1- Create new Pass\n    2- Encrypt existing Pass\n    3- forgot Pass [ID num required]"
+    operationID = []
+    
+    
+    
+"""
+def passGenerator():
+    charSelection = []
+    
+def encryptionAlgo():
+"""
+    
+
+if __name__ == '__main__':
+    main()
+    
